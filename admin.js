1f70bf22aafce1ff789e861f4c520e7037bb47aa
Nikhil kumar <89766775+Nikhilnetha119@users.noreply.github.com>
2024-02-07 19:48:00 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,15 @@
+var AddProduct  = document.getElementById('button')
+
+AddProduct.addEventListener('click', function() {
+    var price = document.getElementById('Price');
+    var Name = document.getElementById('Name');
+
+    AddProduct = {
+        price : price,
+        Name : Name
+    }
+})
+
+function {
+    
+}
